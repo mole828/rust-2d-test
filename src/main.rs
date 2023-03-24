26bb79c0fb04daf89c295b2512eb237c675466d2
mole828 <mrx800@163.com>
2023-03-24 15:58:48 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+extern crate piston_window;
+extern crate find_folder;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
-    let a = 1;
-    println!(a);
+    let opengl = OpenGL::V3_2;
+    let mut window: PistonWindow =
+        WindowSettings::new("piston: image", [964, 878])
+        .exit_on_esc(true)
+        .graphics_api(opengl)
+        .build()
+        .unwrap();
+
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder("assets").unwrap();
+    let rust_logo = assets.join("截屏2023-03-15 14.59.40.png");
+    let rust_logo: G2dTexture = Texture::from_path(
+            &mut window.create_texture_context(),
+            &rust_logo,
+            Flip::None,
+            &TextureSettings::new()
+        ).unwrap();
+    window.set_lazy(true);
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g, _| {
+            clear([1.0; 4], g);
+            image(&rust_logo, c.transform, g);
+        });
+    }
 }
